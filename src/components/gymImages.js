ef2b78a3629adbfd3f54a115b5b1711104d7f52e
steven <szapata427@yahoo.com>
2019-06-20 23:19:31 -0400

    formated the code anad adjusted the css for some of the gym information on the home page

--- src/components/gymImages.js
+++ src/components/gymImages.js
@@ -1,67 +1,65 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 // import PriceRange from './priceRangeSearch'
 
-
-
 class GymImages extends Component {
+  gymImagesProps = () => {
+    let allgyminfo = this.props.gyminfo;
+    let allgymimages = this.props.gymImages;
 
-
-
-    
-gymImagesProps = () => {
-  let allgyminfo = this.props.gyminfo
-  let allgymimages = this.props.gymImages
-
-let mgymsarray = []
+    let mgymsarray = [];
     if (allgymimages && allgyminfo) {
+      this.props.gyminfo.filter(gym => {
+        allgymimages.map(gymimage => {
+          if (gymimage["gym_id"] == gym["id"]) {
+            let firstletteruppercasevalue = gym.value.charAt(0).toUpperCase();
+            let finalvalue = firstletteruppercasevalue + gym.value.slice(1);
+            let matchingobject = {
+              url: gymimage.mainImageUrl,
+              name: gym.name,
+              value: finalvalue,
+              locations: gym.numLocations
+            };
+            mgymsarray.push(matchingobject);
+          }
+        });
+      });
 
-       this.props.gyminfo.filter(gym => {
-                    allgymimages.map(gymimage => {
-                  if (gymimage["gym_id"] == gym["id"]) {
-                      let firstletteruppercasevalue = gym.value.charAt(0).toUpperCase()
-                      let finalvalue = firstletteruppercasevalue + gym.value.slice(1)
-                    let matchingobject =  {url: gymimage.mainImageUrl, name: gym.name, value: finalvalue, locations: gym.numLocations}
-                    mgymsarray.push(matchingobject)
-                  }
-              }) 
-              
-          })
-    
-          return mgymsarray.map(gyminfo => {
-              return <div class="home-page-image-container">
-                 <img class="home-page-gym-logoimage" src={gyminfo.url}/>
-                       <div class="individual-gym-info-container-home-page"> 
-                           <ul class="individual-gym-info-home-page" id="individual-gym-info-name-home-page">
-                               {gyminfo.name}
-                           </ul>
-                           <ul class="individual-gym-info-home-page">
-                           <label class="label-gyminfo-home-page">Value:</label>
-                               {gyminfo.value}
-                           </ul>
-                           <ul class="individual-gym-info-home-page">
-                           <label class="label-gyminfo-home-page"># of Locations:</label>
-                               {gyminfo.locations}
-                           </ul>
-                       </div>
-                   </div> 
-          })
-}
+      return mgymsarray.map(gyminfo => {
+        return (
+          <div class="home-page-image-container">
+            <img class="home-page-gym-logoimage" src={gyminfo.url} />
+            <div class="individual-gym-info-container-home-page">
+              <ul
+                class="individual-gym-info-home-page"
+                id="individual-gym-info-name-home-page"
+              >
+                {gyminfo.name}
+              </ul>
+              <ul class="individual-gym-info-home-page">
+                <label class="label-gyminfo-home-page">Value:</label>
+                {gyminfo.value}
+              </ul>
+              <ul class="individual-gym-info-home-page">
+                <label class="label-gyminfo-home-page"># of Locations:</label>
+                {gyminfo.locations}
+              </ul>
+            </div>
+          </div>
+        );
+      });
+    }
+  };
 
-  
-}
-
-
-
-
-render() {
+  render() {
     return (
-        <React.Fragment>
-            <div class="selected-price-popup">You have Selected a budget of ${this.props.price}</div>
-            {this.gymImagesProps()}
-        </React.Fragment>
-        
-    )
-}
+      <React.Fragment>
+        <div class="selected-price-popup">
+          You have Selected a budget of ${this.props.price}
+        </div>
+        {this.gymImagesProps()}
+      </React.Fragment>
+    );
+  }
 }
 
-export default GymImages
+export default GymImages;
--- src/components/priceRangeSearch.js
+++ src/components/priceRangeSearch.js
@@ -1,96 +1,89 @@
 import React, { Component } from "react";
-import GymImages from './gymImages'
+import GymImages from "./gymImages";
 
 class PriceRange extends Component {
-  
   state = {
     price: 100,
     gymImages: "",
     gymInfo: ""
   };
 
-
-
-
   sliderValue = e => {
-    this.setState(
-      {
-        price: e.target.value
-      }
-    );
+    this.setState({
+      price: e.target.value
+    });
   };
 
-
   handleBudgetSubmit = (e, price) => {
-      e.preventDefault()
+    e.preventDefault();
 
     fetch(`http://localhost:4000/images`)
-    .then(response => response.json())
-    .then(data => {
-        let gymimages = data.data 
+      .then(response => response.json())
+      .then(data => {
+        let gymimages = data.data;
 
         this.setState({
           gymImages: gymimages
-        })
-        
-      })
+        });
+      });
     fetch(`http://localhost:4000/gyms`)
-    .then(response => response.json())
-    .then(data => {
-        let gymdata = data.data 
+      .then(response => response.json())
+      .then(data => {
+        let gymdata = data.data;
 
         this.setState({
           gymInfo: gymdata
-        })
-        
-      })
+        });
+      });
+  };
 
-
-
-    }
-    
-    
-    
-    
-    render() {
-
+  render() {
     return (
-      
-        <React.Fragment>
-          
+      <React.Fragment>
         <div class="main-container-price-range-home">
-        <div class="container-monthly-price-range-title-home">
-          <label for="volume" id="monthly-price-range-title-home">
-            Monthly Price Range
-          </label>
-        </div>
-        <div class="sliderContainer">
-          <span id="lowest-range" class="range-prices">
-            $10
-          </span>
-          <input
-            type="range"
-            id="start"
-            name="volume"
-            min="10"
-            max="300"
-            class="slider"
-            onChange={e => this.sliderValue(e)}
-            value={this.state.value}
-          />
-          <span id="highest-range" class="range-prices">
-            $300
-          </span>
-          <div>
-            <span id="budget-price-home-page"> ${this.state.price}</span>
+          <div class="container-monthly-price-range-title-home">
+            <label for="volume" id="monthly-price-range-title-home">
+              Monthly Price Range
+            </label>
           </div>
-          <button class="monthly-price-range-submitbtn" onClick={(e, state) => this.handleBudgetSubmit(e, this.state.price)}>Select Your Budget!</button>
-
-        
+          <div class="sliderContainer">
+            <span id="lowest-range" class="range-prices">
+              $10
+            </span>
+            <input
+              type="range"
+              id="start"
+              name="volume"
+              min="10"
+              max="300"
+              class="slider"
+              onChange={e => this.sliderValue(e)}
+              value={this.state.value}
+            />
+            <span id="highest-range" class="range-prices">
+              $300
+            </span>
+            <div>
+              <span id="budget-price-home-page"> ${this.state.price}</span>
+            </div>
+            <button
+              class="monthly-price-range-submitbtn"
+              onClick={(e, state) =>
+                this.handleBudgetSubmit(e, this.state.price)
+              }
+            >
+              Select Your Budget!
+            </button>
+          </div>
+          {this.state.gymImages ? (
+            <GymImages
+              gymImages={this.state.gymImages}
+              price={this.state.price}
+              gyminfo={this.state.gymInfo}
+            />
+          ) : null}
         </div>
-        {this.state.gymImages ? <GymImages gymImages={this.state.gymImages} price={this.state.price}  gyminfo={this.state.gymInfo}/> : null}
-      </div>
-        </React.Fragment>
+      </React.Fragment>
     );
   }
 }
