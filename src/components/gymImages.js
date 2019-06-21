c289992b927430d01b72209a318f9ceca1966a14
steven <szapata427@yahoo.com>
2019-06-20 21:11:44 -0400

    add div to the gym information

--- src/components/gymImages.js
+++ src/components/gymImages.js
@@ -9,43 +9,39 @@ class GymImages extends Component {
 
     
 gymImagesProps = () => {
-
   let allgyminfo = this.props.gyminfo
   let allgymimages = this.props.gymImages
-  console.log(allgyminfo)
-  console.log(allgymimages)
-
-
 
 let mgymsarray = []
-   this.props.gyminfo.filter(gym => {
-                allgymimages.map(gymimage => {
-              if (gymimage["gym_id"] == gym["id"]) {
-                let matchingobject =  {url: gymimage.mainImageUrl, name: gym.name, value: gym.value, locations: gym.numLocations}
-                mgymsarray.push(matchingobject)
-              }
-          })
-      
-          
-          
-      })
+    if (allgymimages && allgyminfo) {
 
-      return mgymsarray.map(gyminfo => {
-          return <div class="home-page-image-container">
-             <img class="home-page-gym-logoimage" src={gyminfo.url}/>
-                   <div>
-                       <ul>
-                           {gyminfo.name}
-                       </ul>
-                       <ul>
-                           {gyminfo.value}
-                       </ul>
-                       <ul>
-                           {gyminfo.locations}
-                       </ul>
-                   </div>
-               </div> 
-      })
+       this.props.gyminfo.filter(gym => {
+                    allgymimages.map(gymimage => {
+                  if (gymimage["gym_id"] == gym["id"]) {
+                    let matchingobject =  {url: gymimage.mainImageUrl, name: gym.name, value: gym.value, locations: gym.numLocations}
+                    mgymsarray.push(matchingobject)
+                  }
+              }) 
+              
+          })
+    
+          return mgymsarray.map(gyminfo => {
+              return <div class="home-page-image-container">
+                 <img class="home-page-gym-logoimage" src={gyminfo.url}/>
+                       <div class="individual-gym-info-container-home-page"> 
+                           <ul class="individual-gym-info-home-page">
+                               {gyminfo.name}
+                           </ul>
+                           <ul class="individual-gym-info-home-page">
+                               {gyminfo.value}
+                           </ul>
+                           <ul class="individual-gym-info-home-page">
+                               {gyminfo.locations}
+                           </ul>
+                       </div>
+                   </div> 
+          })
+}
 
   
 }
